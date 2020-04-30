bc9b6e3da66698382c0d299a690395925055430c
Crowdbotics <team@crowdbotics.com>
2020-04-30 23:48:12 +0000

    Update models

--- backend/mytyme2shine_16469/wsgi.py
+++ backend/mytyme2shine_16469/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mytyme2shine_16469.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mytyme2shine_16469.settings")
 
 application = get_wsgi_application()
